28ed339d232138f3f5db46ded8e95b804d55600d
The Buildbot <buildbot@angular2buch.de>
2017-01-14 10:27:47 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-buch/book-monkey2/commit/ae7bb9544ef80108b0e02ae8cd9eb57286849593
    Travis build: https://travis-ci.org/angular-buch/book-monkey2/builds/191881995

--- 1.chunk.js
+++ 1.chunk.js
@@ -405,7 +405,7 @@ module.exports = "<img class=\"ui tiny image\"\n     *ngIf=\"book.thumbnails\"\n
 /***/ 715:
 /***/ function(module, exports) {
 
-module.exports = "<div class=\"ui middle aligned selection divided list\">\n  <a class=\"bm-book-list-item item\"\n      *ngFor=\"let b of booksOb | async\"\n      [book]=\"b\"\n      [routerLink]=\"b.isbn\"></a>\n  <div *ngIf=\"!booksOb\" class=\"ui active dimmer\">\n    <div class=\"ui large text loader\">Daten werden geladen...</div>\n  </div>\n  <p *ngIf=\"booksOb && !booksOb.length\">Es wurden noch keine B&uuml;cher eingetragen.</p>\n</div>\n"
+module.exports = "<div class=\"ui middle aligned selection divided list\"\n     *ngIf=\"booksOb | async; else loading; let books\">\n  <a class=\"bm-book-list-item item\"\n     *ngFor=\"let b of books\"\n     [book]=\"b\"\n     [routerLink]=\"b.isbn\"></a>\n  <p *ngIf=\"!books.length\">Es wurden noch keine B&uuml;cher eingetragen.</p>\n</div>\n<template #loading>\n  <div class=\"ui active dimmer\">\n    <div class=\"ui large text loader\">Daten werden geladen...</div>\n  </div>\n</template>\n"
 
 /***/ }
 
