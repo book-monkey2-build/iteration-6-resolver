01379f46e58ca1ea9ed76838e2e758cee58e1a55
The Buildbot <buildbot@angular2buch.de>
2017-01-22 13:56:16 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-buch/book-monkey2/commit/ecfcf9559675f6524369140bdcdf135ff586c45a
    Travis build: https://travis-ci.org/angular-buch/book-monkey2/builds/194207967

--- 1.chunk.js
+++ 1.chunk.js
@@ -138,7 +138,7 @@ var BookListComponent = (function () {
         this.bs = bs;
     }
     BookListComponent.prototype.ngOnInit = function () {
-        this.booksOb = this.bs.getAll();
+        this.books$ = this.bs.getAll();
     };
     BookListComponent = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
@@ -405,7 +405,7 @@ module.exports = "<img class=\"ui tiny image\"\n     *ngIf=\"book.thumbnails\"\n
 /***/ 715:
 /***/ function(module, exports) {
 
-module.exports = "<div class=\"ui middle aligned selection divided list\"\n     *ngIf=\"booksOb | async; else loading; let books\">\n  <a class=\"bm-book-list-item item\"\n     *ngFor=\"let b of books\"\n     [book]=\"b\"\n     [routerLink]=\"b.isbn\"></a>\n  <p *ngIf=\"!books.length\">Es wurden noch keine B&uuml;cher eingetragen.</p>\n</div>\n<template #loading>\n  <div class=\"ui active dimmer\">\n    <div class=\"ui large text loader\">Daten werden geladen...</div>\n  </div>\n</template>\n"
+module.exports = "<div class=\"ui middle aligned selection divided list\"\n     *ngIf=\"books$ | async; else loading; let books\">\n  <a class=\"bm-book-list-item item\"\n     *ngFor=\"let b of books\"\n     [book]=\"b\"\n     [routerLink]=\"b.isbn\"></a>\n  <p *ngIf=\"!books.length\">Es wurden noch keine B&uuml;cher eingetragen.</p>\n</div>\n<template #loading>\n  <div class=\"ui active dimmer\">\n    <div class=\"ui large text loader\">Daten werden geladen...</div>\n  </div>\n</template>\n"
 
 /***/ }
 
